1b7aa459b378bd82cdca625684352f57d90966d8
beldon <1047325205@qq.com>
2021-07-10 17:01:24 +0800

    add demo

--- src/main/java/com/fujfu/template/TemplateApplication.java
+++ src/main/java/com/fujfu/template/TemplateApplication.java
@@ -2,13 +2,8 @@ package com.fujfu.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableSwagger2
-// TODO: 请将 TemplateApplication 修改为自己的应用名称
 public class TemplateApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/fujfu/template/config/CrossConfig.java
@@ -0,0 +1,18 @@
+package com.fujfu.template.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrossConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
